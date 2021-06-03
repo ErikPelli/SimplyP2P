c6fe24feda0d54c2bb50ab1c0c7bd3a8f6ecb11a
ErikPelli <erikpellizzon1@gmail.com>
2021-06-03 22:34:33 +0200

    Test executable
    Created a test main file that create 6 peers that interact with each other.

--- cmd/localTest.go
+++ cmd/localTest.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"github.com/ErikPelli/SimplyP2P"
+	"sync"
+)
 
+func main() {
+	var wg sync.WaitGroup
+
+	listenPorts := []string{"2020", "2021", "2022", "2023", "2024", "2025"}
+	peers := [][][]string{
+		{},
+		{{"", "2020"}},
+		{{"127.0.0.1", "2020"}, {"127.0.0.1", "2021"}},
+		{{"127.0.0.1", "2020"}, {"127.0.0.1", "2022"}},
+		{{"127.0.0.1", "2021"}, {"127.0.0.1", "2023"}},
+		{{"127.0.0.1", "2020"}, {"127.0.0.1", "2022"}},
+	}
+
+	// Create 6 P2P peers
+	for i, port := range listenPorts {
+		if _, err := SimplyP2P.NewNode(port, peers[i], &wg); err != nil {
+			panic(err)
+		}
+	}
+
+	wg.Wait()
 }
