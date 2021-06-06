180167f70c18f9e104e4a4f162b3c20ac411b8ed
ErikPelli <erikpellizzon1@gmail.com>
2021-06-06 17:27:57 +0200

    atomic peers counter
    Now peers counter is an atomic int32 instead of an int with a mutex, to improve performance.

--- peers.go
+++ peers.go
@@ -5,41 +5,52 @@ import (
 	"fmt"
 	"io"
 	"sync"
+	"sync/atomic"
 )
 
-// Peers is a map that contains all connected peers
-type Peers struct {
-	peers      sync.Map
-	counter    int
-	counterMtx sync.Mutex
+// Counter is the number of connected peers.
+type Counter int32
+
+// Increment adds 1 to the value of counter.
+func (c *Counter) Increment() {
+	atomic.AddInt32((*int32)(c), 1)
 }
 
-// Add adds a peer to the map.
+// Decrement subtracts 1 from the value of the counter.
+func (c *Counter) Decrement() {
+	atomic.AddInt32((*int32)(c), ^int32(0))
+}
+
+// Value returns current value of the counter.
+func (c *Counter) Value() int32 {
+	return atomic.LoadInt32((*int32)(c))
+}
+
+// Peers is a map that contains all connected peers.
+type Peers struct {
+	peers       sync.Map
+	peersNumber Counter
+}
+
+// Add adds a peer to the peers map.
 func (p *Peers) Add(key Peer, value *Connection) {
 	_, old := p.peers.Load(key)
 	if !old {
 		// Add peer if not already present
 		p.peers.Store(key, value)
-		p.counterMtx.Lock()
-		p.counter++
-		p.counterMtx.Unlock()
+		p.peersNumber.Increment()
 	}
 }
 
-// Remove removes a peer from the map
+// Remove removes a peer from the peers map.
 func (p *Peers) Remove(key Peer) {
 	if peer, ok := p.peers.LoadAndDelete(key); ok {
-		currentPeer := peer.(*Connection)
-		_ = currentPeer.Close()
-
-		// Decrement counter
-		p.counterMtx.Lock()
-		p.counter--
-		p.counterMtx.Unlock()
+		_ = peer.(*Connection).Close()
+		p.peersNumber.Decrement()
 	}
 }
 
-// Get returns a peer from the map
+// Get returns a peer from the peers map.
 func (p *Peers) Get(key Peer) (*Connection, error) {
 	if peer, ok := p.peers.Load(key); ok {
 		return peer.(*Connection), nil
@@ -49,8 +60,8 @@ func (p *Peers) Get(key Peer) (*Connection, error) {
 }
 
 // Len returns the number of peers connected.
-func (p *Peers) Len() int {
-	return p.counter
+func (p *Peers) Len() int32 {
+	return p.peersNumber.Value()
 }
 
 // Broadcast sends a packet to all peers connected.
@@ -58,18 +69,15 @@ func (p *Peers) Broadcast(to io.WriterTo) {
 	fmt.Println("Sent new broadcast packet")
 
 	p.peers.Range(func(key interface{}, value interface{}) bool {
-		currentPeer := value.(*Connection)
-
 		// Remove current peer if there is a connection problem
-		if _, err := to.WriteTo(currentPeer); err != nil {
+		if _, err := to.WriteTo(value.(*Connection)); err != nil {
 			p.Remove(key.(Peer))
 		}
-
 		return true
 	})
 }
 
-// Close closes all connections.
+// Close closes all connections with other peers.
 func (p *Peers) Close() error {
 	p.peers.Range(func(key interface{}, value interface{}) bool {
 		p.Remove(key.(Peer))
