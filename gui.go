0607896949e7a59afaf4b98da25870584d7ec4bf
ErikPelli <erikpellizzon1@gmail.com>
2021-06-06 18:07:17 +0200

    GUI refactoring
    Added comments.
    Use State SetEvent function to change label value.

--- gui.go
+++ gui.go
@@ -5,13 +5,16 @@ import (
 	"time"
 )
 
+// SimplyGui is a simple graphic interface that show
+// the current state and some buttons to change its value.
 type SimplyGui struct {
 	window *ui.Window
 }
 
+// setupUI sets the gui elements and show then.
 func (n *Node) setupUI() {
 	gui := new(SimplyGui)
-	gui.window = ui.NewWindow("State Peer" , 300, 200, false)
+	gui.window = ui.NewWindow("State Peer", 300, 200, false)
 
 	// Close current Node at exit
 	gui.window.OnClosing(func(*ui.Window) bool {
@@ -26,42 +29,44 @@ func (n *Node) setupUI() {
 	})
 
 	box := ui.NewVerticalBox()
-	gui.window.SetChild(box)
-
-    // Current state label that will be updated
 	state := ui.NewLabel("")
 
-	n.state.event = func(actualState bool) {
+	// Set state change handler (change GUI label)
+	n.state.SetEvent(func(actualState bool) {
 		if actualState {
 			state.SetText("Actual: True")
 		} else {
 			state.SetText("Actual: False")
 		}
-	}
+	})
 
 	// Function that create a new change state button
-	addButton := func(text string, newState bool) *ui.Button {
+	addChangeStateButton := func(text string, newState bool) *ui.Button {
 		button := ui.NewButton(text)
-		button.OnClicked(func(btn *ui.Button) {
+		button.OnClicked(func(*ui.Button) {
 			currentTime := time.Now()
 			if n.state.Update(newState, currentTime) {
 				n.peers.Broadcast(ChangeState{
 					State: newState,
-					time:  currentTime,
+					Time:  currentTime,
 				})
 			}
 		})
-
 		return button
 	}
 
+	// Add graphic elements to box
 	box.Append(state, false)
-	box.Append(addButton("False", false), false)
-	box.Append(addButton("True", true), false)
+	box.Append(addChangeStateButton("False", false), false)
+	box.Append(addChangeStateButton("True", true), false)
 
+	// Add box to the window and show the window
+	gui.window.SetChild(box)
 	gui.window.Show()
 }
 
+// newGui creates a new GUI linked to current node
+// and starts it in a new goroutine.
 func (n *Node) newGui() {
 	go ui.Main(n.setupUI)
 }
